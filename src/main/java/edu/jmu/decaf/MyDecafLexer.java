3b7264f13f72d22baa6f070519c96056f95eab6f
Josh Ehrlich <ehrlicjd@dukes.jmu.edu>
2019-09-21 15:04:48 -0400

    Passing all tests, added mvn files

--- src/main/java/edu/jmu/decaf/MyDecafLexer.java
+++ src/main/java/edu/jmu/decaf/MyDecafLexer.java
@@ -14,8 +14,11 @@ import edu.jmu.decaf.Token.Type;
 class MyDecafLexer extends DecafLexer
 {
 	
-//	String[] keyWordArray = {"def","if", "else", 
-//		"while", "return", "break", "continue", "int", "bool", "void", "true", "false"};
+	/* * * * * * * * * * * * * *
+	 * For this and all future projects, you must also make sure you check the inputs
+	 * to public API functions (in this case, any public members of MyDecafLexer)
+	 * to make sure they aren't null or otherwise invalid
+	 * * * * * * * * * * * * * */
 
     /**
      * Perform lexical analysis, converting Decaf source code into a stream of
@@ -32,6 +35,9 @@ class MyDecafLexer extends DecafLexer
             throws IOException, InvalidTokenException
     {
     	
+    	String []keywords = {"def", "if", "else", "while", "return", "break", "continue", "int", "bool", "void", "true", "false"};
+    	String []reserved = {"for", "callout", "class", "interface", "extends", "implements", "new", "this", "string", "float", "double", "null"};
+    	HashSet<String> keysHash = new HashSet<>();
     	Queue<Token> tokens = new ArrayDeque<Token>();
     	
     	String str = "";
@@ -41,48 +47,111 @@ class MyDecafLexer extends DecafLexer
     	//StringBuffer temp = new StringBuffer();
     	
     	
-    	
+    	/*
+    	 * 
+    	 * \d	A digit: [0-9]
+			\D	A non-digit: [^0-9]
+			\s	A whitespace character: [ \t\n\x0B\f\r]
+			\S	A non-whitespace character: [^\s]
+			\w	A word character: [a-zA-Z_0-9]
+			\W	A non-word character: [^\w]
+    	 */
     	
     	  //Regex's --> 
-        Pattern identifier = Pattern.compile("[a-zA-z][a-zA-Z_0-9]*"); //Cant start with an _ --> Correct???
-        Pattern whitespace = Pattern.compile("([\\s+])|(\\t)");
-        Pattern keyword = Pattern.compile("def|if|else|while|return|break|continue|int|bool|void|true|false"); // Can keywords have _ --> ???? This may violate the rules for keywords
+    	Pattern whitespace = Pattern.compile("[\\s\\t\\n]+");
+    	Pattern keyword = Pattern.compile("(def|if|else|while|return|break|continue|int|bool|void|true|false)"); // Can keywords have _ --> ???? This may violate the rules for keywords
+        Pattern identifier = Pattern.compile("[a-zA-z][a-zA-Z0-9_]*"); //Cant start with an _ --> Correct???
         Pattern decimalLiteral = Pattern.compile("[0-9][1-9]*");
         Pattern hexLiteral = Pattern.compile("0x[A-F0-9]*");
-        Pattern stringLiteral = Pattern.compile("\"[a-zA-Z0-9]*\"");
-        Pattern symbol = Pattern.compile("[-\\[\\]\\(\\{\\}\\)\\,\\;\\=\\+\\-\\*\\/\\%\\<\\>\\<=\\>=\\==\\!=\\&&\\||\\!]"); // will this allow  <, <=, and = in the same regex without confusion(prob not)
-       
+        Pattern stringLiteral = Pattern.compile("\"[a-zA-Z0-9]+\"");
+        Pattern symbol = Pattern.compile("\\/\\/|[(){}//[//]]|[=|<|>|!]?=|[+8\\/\\-<>!,\\;]|\\|\\||%|\\&\\&"); // will this allow  <, <=, and = in the same regex without confusion(prob not)
+        //Pattern comments = Pattern.compile("(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|(//.*)");
         //Token Patterns --> ORDER MATTERS --> This order will probably need to be rearranged once tested
-        addTokenPattern(Token.Type.KEY, keyword.toString());
+        
         addTokenPattern(Token.Type.ID, identifier.toString());
+        addTokenPattern(Token.Type.KEY, keyword.toString());
         addTokenPattern(Token.Type.HEX, hexLiteral.toString());
         addTokenPattern(Token.Type.DEC, decimalLiteral.toString());
+       // addTokenPattern(null, comments.toString());
         addTokenPattern(Token.Type.SYM, symbol.toString());
         addTokenPattern(Token.Type.STR, stringLiteral.toString());
+        
+       // addIgnoredPattern(comments.toString());
+        addIgnoredPattern(whitespace.toString());
         
       
         //Token token = new Token(this.nextToken(sb).type, sb.toString(), si);
        
-        //Ignored Patterns
-       
+        
+//        for(int i = 0; i < keywords.length; i++)
+//        {
+//        	keysHash.add(keywords[i]);
+//        }
         
         while((str = input.readLine()) != null)
         {
-        	 
-        	 extract(whitespace, sb);
+        	 SourceInfo si = new SourceInfo(filename);
+        	 si.lineNumber = lineNumber;
+        	
         	 sb.append(str); // Do we want to add all lines to string buffer than do work or do work line by line as we read?
+        	 extract(whitespace, sb);
+        	
         	 while((token = nextToken(sb)) != null)
         	 {
-        		 extract(whitespace, sb);
-        		 SourceInfo si = new SourceInfo(filename, lineNumber); //does this need to be a full path? with file extension(.decaf)? Yes
+        		 if(token.text.equals("//")) //if a token is // you know its a comment so delete the entire line therefore ignoring the comment
+        		 {
+        			// System.out.println("Comment");
+        			 sb.delete(0, sb.length());
+        			 continue;
+        		 }
         		 token.source = si;
-        		 tokens.add(new Token(token.type, token.text, token.source));
         		 
-        		 
+        		
+        		if(token.type == Type.ID) // Checks if an ID is a keyword or reserved word
+        		{
+        			for(int i = 0; i < keywords.length; i ++)
+           		 	{
+        				if(keywords[i].equals(token.text))
+        				{
+        					token.type = Type.KEY;
+        				}
+           		 	}
+        			
+        			for(int i = 0; i < reserved.length; i ++)
+        			{
+        				if(reserved[i].equals(token.text))
+        				{
+        					throw new InvalidTokenException("Use of a reserved word as an ID");
+        				}
+        			}
+        		}
+        		
+        		tokens.add(new Token(token.type, token.text, token.source));
+        		
+        		extract(whitespace, sb);
         	 }
-        	lineNumber++;
+        	 if(token == null && sb.length() != 0)
+        	 {
+        		//System.out.println("Hello" + lineNumber);
+        		 throw new InvalidTokenException("Invalid token");
+        	 }
+        	 
+        	 lineNumber++;        	
         }
         //System.out.println("End Loop");
+       
+        Iterator<Token> iterator = tokens.iterator();
+        while(iterator.hasNext())
+        {
+        	token = iterator.next();
+        	//System.out.println("Iterator -->" + token);
+        	if(token == null)
+        	{
+        		tokens.remove(token); // I dont think i do this
+        		throw new InvalidTokenException("Invalid token");
+        	}
+        	System.out.println();
+        }
        System.out.println("Tokens -->" + tokens.size());
         return tokens;
     }
